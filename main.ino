8198e92f000013827252770579b5dfcbb91353ed
HoRuiEn <ernie.birdy.ho@gmail.com>
2020-10-22 22:27:49 +0800

    Fix switch.

--- main.ino
+++ main.ino
@@ -7,14 +7,24 @@ void actuate(char receivedByte) {
     switch (receivedByte) {
         case 'P':
             servoPlaySequence();
+            Serial.println("play sequence");
+            break;
         case 'R':
             servoRecordSequence();
+            Serial.println("record sequence");
+            break;
         case 'T':
             servoTestPosition();
+            Serial.println("test sequence");
+            break;
         case 'S':
             servoSaveRecordedSequence();
+            Serial.println("save sequence");
+            break;
         case 'C':
             servoClearSequenceArray();
+            Serial.println("clear sequence");
+            break;
         default:
             // Serial.println("Invalid command");
             validInput = false;
@@ -36,5 +46,5 @@ void setup() {
 }
 
 void loop() {
-    zeroAllServos();
+    listenAndActuate();
 }
