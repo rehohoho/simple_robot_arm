ab054248155426c28b0d6c767402ab52d3d83925
iangohy <ian@iangohy.com>
2020-10-30 10:12:00 +0800

    Added move_gently_2 function

--- servo_manual_input/servo_manual_input.ino
+++ servo_manual_input/servo_manual_input.ino
@@ -7,6 +7,7 @@ constexpr int servo3_pin = 10;
 
 constexpr int interval = 10;
 constexpr float cheat_factor = 0;
+constexpr int MOTION_TIME = 2000;
 
 int pos[4] = {10, 140, 180, 180};
 
@@ -62,8 +63,52 @@ void move_gently(int final_pos [4])
   }
 }
 
+void move_gently_2(int final_pos [4]) {
+  int motion_time = MOTION_TIME;
+  int motion_start = millis();
 
-void setup() {
+  int start_pos_arr [4] {};
+  for (int i {0}; i < 4; ++i)
+  {
+    start_pos_arr[i] = pos[i];
+  }
+
+  while (pos[0] != final_pos[0] || pos[1] != final_pos[1] || pos[2] != final_pos[2] || pos[3] != final_pos[3])
+  {
+    for (int i {0}; i < 4; ++i) 
+    {
+      int current_time = millis() - motion_start;
+      int angle = motion_curve(start_pos_arr[i], final_pos[i], motion_time, current_time);
+      pos[i] = angle;
+    } 
+    servo0.write(pos[0]);
+    servo1.write(pos[1]);
+    servo2.write(pos[2]);
+    servo3.write(pos[3]);
+    Serial.print("Moving: "); Serial.print(pos[0]); Serial.print(" ");
+    Serial.print(pos[1]); Serial.print(" "); Serial.print(pos[2]);
+    Serial.print(" "); Serial.println(pos[3]);
+  }
+}
+
+float motion_curve(int start_pos, int end_pos, int motion_time, int current_time)
+{
+  if (current_time > motion_time)
+  {
+    return end_pos;
+  }
+  int a = start_pos;
+  int b = end_pos;
+  int t = motion_time;
+  int x = current_time;
+
+  float c = (b-a) / 2;
+  float result = c * (cbrt((x - (t/2.0))/(t/2.0))) + c + a;
+  return result;
+}
+
+void setup() 
+{
   Serial.begin(9600);
   servo0.attach(servo0_pin);
   servo1.attach(servo1_pin);
@@ -74,9 +119,12 @@ void setup() {
   servo1.write(pos[1]);
   servo2.write(pos[2]);
   servo3.write(pos[3]);
+
+  Serial.println("READY");
 }
 
-void loop() {
+void loop() 
+{
   if (Serial.available() > 0) {
     int angle_0 = Serial.parseInt();
     int angle_1 = Serial.parseInt();
@@ -93,6 +141,6 @@ void loop() {
     
     // serialFlush();
     int new_pos [4] {angle_0, angle_1, angle_2, angle_3};
-    move_gently(new_pos);
+    move_gently_2(new_pos);
   }
 }
